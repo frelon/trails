ee38656a002abb19527986f9b26122b035327e36
frelon <fredrik.lonnegren@gmail.com>
2018-01-03 21:41:54 +0100

    Using the old view for landing screen

--- Trails.Web/HikeApp/src/app/app.module.ts
+++ Trails.Web/HikeApp/src/app/app.module.ts
@@ -3,11 +3,12 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
-
+import { WelcomeComponent } from './welcome/welcome.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WelcomeComponent
   ],
   imports: [
     BrowserModule
