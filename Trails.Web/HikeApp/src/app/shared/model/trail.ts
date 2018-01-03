33a3a1f2c5041536693ade846f0828f7fccfc87b
frelon <fredrik.lonnegren@gmail.com>
2018-01-03 18:13:50 +0100

    Removing .js files and adding a shared folder for the ts model

---
+++ Trails.Web/HikeApp/src/app/shared/model/trail.ts
@@ -0,0 +1,17 @@
+﻿export class Trail {
+    constructor(Id: number) {
+        this.Id = Id;
+        this.Points = new Array<Point>();
+    }
+
+    public Id: number;
+    public Name: string;
+    public Points: Point[];
+}
+
+export class Point {
+    public Latitude: number;
+    public Longitude: number;
+    public Altitude: number;
+    public TimeStamp: Date;
+}
